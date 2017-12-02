76573a02107e15d587ad21e0437bd947aa7cd2e6
yu Ichinose <yoshii.hisataka@gmail.com>
2017-12-03 00:41:04 +0900

    -a

--- src/main/java/com/entity/Ranking.java
+++ src/main/java/com/entity/Ranking.java
@@ -11,5 +11,3 @@ public class Ranking {
   @Setter
   private String username;
 }
-
-  <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1VpBezAnqb8rW8ebGZImDHjfVhCFERTwB" width="640" height="480"></iframe>
