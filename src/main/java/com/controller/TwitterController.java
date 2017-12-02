5342d57235699a7647888a1e65c3c192bff2c86e
yu Ichinose <yoshii.hisataka@gmail.com>
2017-12-02 15:53:04 +0900

    a

--- src/main/java/com/controller/TwitterController.java
+++ src/main/java/com/controller/TwitterController.java
@@ -140,7 +140,7 @@ public class TwitterController {
 
   @RequestMapping(value="/getRanking")
 	@ResponseBody
-	public String getRankingList(@ModelAttribute TwitterForm form, Model model) {
+	public String getRanking(@ModelAttribute TwitterForm form, Model model) {
     try {
 
       String sql = "select rank from (select row_number() over(order by point desc) rank, username, point from (select username, max(point) as point from medi group by username) A order by point desc) A where username = ";
