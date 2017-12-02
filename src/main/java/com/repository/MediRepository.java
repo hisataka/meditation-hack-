f2eff4f3b6abe58094dc299d37c5a9b3ebd776ce
yu Ichinose <yoshii.hisataka@gmail.com>
2017-12-02 11:53:55 +0900

    a

--- src/main/java/com/repository/MediRepository.java
+++ src/main/java/com/repository/MediRepository.java
@@ -2,7 +2,7 @@ package com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.entity.Medi;
+import com.entity.*;
 import java.util.List;
 
 public interface MediRepository extends JpaRepository<Medi, MediKey> {
