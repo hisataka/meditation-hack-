5c8b27c1ca39ec4f40df1fd9d80b214e2c69431d
yu Ichinose <yoshii.hisataka@gmail.com>
2017-12-02 14:06:17 +0900

    a

--- src/main/java/com/repository/MediRepository.java
+++ src/main/java/com/repository/MediRepository.java
@@ -1,6 +1,7 @@
 package com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.entity.Medi;
 import java.util.List;
