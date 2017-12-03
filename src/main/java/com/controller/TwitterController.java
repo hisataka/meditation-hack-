e15646015cd8e406e7d03a6bba64ea4ef4a4a094
yu Ichinose <yoshii.hisataka@gmail.com>
2017-12-03 10:22:42 +0900

    -a

--- src/main/java/com/controller/TwitterController.java
+++ src/main/java/com/controller/TwitterController.java
@@ -165,18 +165,18 @@ public class TwitterController {
   @RequestMapping(value="/getRanking2")
 	@ResponseBody
 	public String getRanking2(@ModelAttribute TwitterForm form, Model model) {
-    try {
+//    try {
 
       String sql = "select point from (select row_number() over(order by point desc) rank, username, point from (select username, max(point) as point from medi group by username) A order by point desc) A where username = '";
       sql += form.getUsername();
       sql += "' ";
 
       BigInteger results = (BigInteger)entityManager.createNativeQuery(sql).getSingleResult();
-
-     return results.toString();
-    } catch(Exception e) {
-      return "false";
-    }
+return results;
+//     return results.toString();
+  //  } catch(Exception e) {
+//      return "false";
+ //   }
 	}
 
 
